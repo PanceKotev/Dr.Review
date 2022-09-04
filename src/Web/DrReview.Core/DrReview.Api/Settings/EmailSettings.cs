72334657158d9ddd3491d44c9756db7790a8b75e
PanceKotev <pance-kotev@hotmail.com>
2022-09-04 20:00:48 +0200

    Remove sendgrid keys from code

--- src/Web/DrReview.Core/DrReview.Api/Settings/EmailSettings.cs
+++ src/Web/DrReview.Core/DrReview.Api/Settings/EmailSettings.cs
@@ -11,6 +11,11 @@
 
         public string FromEmail => GetValue<string>("EmailSettings:FromEmail");
 
-        public string ApiKey => GetValue<string>("EmailSettings:ApiKey");
+        public string ApiKey =>
+#if DEBUG
+            Environment.GetEnvironmentVariable("DRREVIEW_SENDGRID_API_KEY")!;
+#else
+GetValue<string>("EmailSettings:ApiKey");
+#endif
     }
 }
