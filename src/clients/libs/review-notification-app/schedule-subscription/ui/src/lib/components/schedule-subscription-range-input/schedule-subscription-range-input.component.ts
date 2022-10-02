09f195dbbac6ebd5b8f6922f3f04813cd73afe21
PanceKotev <pance-kotev@hotmail.com>
2022-10-02 20:59:44 +0200

    Before update

--- src/clients/libs/review-notification-app/schedule-subscription/ui/src/lib/components/schedule-subscription-range-input/schedule-subscription-range-input.component.ts
+++ src/clients/libs/review-notification-app/schedule-subscription/ui/src/lib/components/schedule-subscription-range-input/schedule-subscription-range-input.component.ts
@@ -1,8 +1,15 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component,
-  forwardRef, OnInit, OnDestroy, Input, OnChanges, SimpleChanges } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  forwardRef,
+  OnInit,
+  OnDestroy,
+  Input
+} from '@angular/core';
 import { BaseControlValueAccessor } from '@drreview/shared/utils/form';
 import { FormControl, FormGroup, NG_VALUE_ACCESSOR } from '@angular/forms';
-import { Subject, takeUntil } from 'rxjs';
+import { debounceTime, Subject, takeUntil } from 'rxjs';
 import { ThemesService } from '@drreview/shared/services/themes';
 import { ScheduleNotificationRange } from '@drreview/shared/data-access';
 
@@ -10,60 +17,60 @@ import { ScheduleNotificationRange } from '@drreview/shared/data-access';
   selector: 'drreview-schedule-subscription-range-input',
   templateUrl: './schedule-subscription-range-input.component.html',
   styleUrls: ['./schedule-subscription-range-input.component.scss'],
-  providers: [{ provide: NG_VALUE_ACCESSOR, useExisting: forwardRef(() => ScheduleSubscriptionRangeInputComponent), multi: true }],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => ScheduleSubscriptionRangeInputComponent),
+      multi: true
+    }
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ScheduleSubscriptionRangeInputComponent extends BaseControlValueAccessor<ScheduleNotificationRange>
-      implements OnInit, OnChanges, OnDestroy {
-
+export class ScheduleSubscriptionRangeInputComponent
+  extends BaseControlValueAccessor<ScheduleNotificationRange | null | undefined>
+  implements OnInit, OnDestroy {
   private isDestroying$ = new Subject<boolean>();
 
   public fg: FormGroup = new FormGroup({
     from: new FormControl<Date | undefined | null>(null),
     to: new FormControl<Date | undefined | null>(null),
-    subscribedTo: new FormControl<boolean>(false)
+    subscribedTo: new FormControl<boolean | undefined>(false)
   });
 
   @Input()
   public notificationSwitchUnder = true;
 
-
   @Input()
   public appearance: 'fill' | 'standard' | 'outline' | 'legacy' = 'fill';
 
-  public override writeValue(obj: ScheduleNotificationRange): void {
-    this.value = obj;
-    this.fg.patchValue(obj);
-    this.cdr?.markForCheck();
+  public override writeValue(
+    obj: ScheduleNotificationRange | undefined | null
+  ): void {
+      // this.value = obj;
+      this.fg.patchValue({
+        from: obj?.from,
+        to: obj?.to,
+        subscribedTo: !!obj?.subscribedTo
+      }, { emitEvent: false, onlySelf: true });
+      this.fg.updateValueAndValidity({ emitEvent: false, onlySelf: true });
+      this.cdr?.markForCheck();
+
   }
 
-  public constructor(public override cdr: ChangeDetectorRef, public themeService: ThemesService) {
+  public constructor(
+    public override cdr: ChangeDetectorRef,
+    public themeService: ThemesService
+  ) {
     super(cdr);
   }
 
   public ngOnInit(): void {
-    if (this.value) {
-      this.fg.patchValue(this.value);
-      this.fg.updateValueAndValidity({emitEvent: false});
-    };
-
-    this.fg.valueChanges.pipe(takeUntil(this.isDestroying$)).subscribe(c => {
+    this.fg.valueChanges.pipe(debounceTime(250),takeUntil(this.isDestroying$)).subscribe((c) => {
+      console.log('ooh value change', c);
       this.onChange(c);
     });
   }
 
-  public ngOnChanges(changes: SimpleChanges): void {
-      if(changes['value']){
-        console.log('cange');
-
-        if (this.value) {
-
-          this.fg.patchValue(this.value);
-          this.fg.updateValueAndValidity({emitEvent: false});
-
-        };
-      }
-  }
   public ngOnDestroy(): void {
     this.isDestroying$.next(true);
     this.isDestroying$.complete();
